d281c3b3eb7d11dd571a023b71d5a334f9487e0c
Pedro Brito <pedrorichillbia@gmail.com>
2025-05-24 19:29:44 -0300

    feat: adiciona logger padrão para aplicação

---
+++ app/core/logger.py
@@ -0,0 +1,9 @@
+import logging
+
+# Configuração básica do logger
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+
+logger = logging.getLogger("lu_estilo_api")
