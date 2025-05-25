dee57772512a97523ba08175352f987d9b5995ae
Pedro Brito <pedrorichillbia@gmail.com>
2025-05-24 21:17:03 -0300

    feat: troca para variavel de ambiente

--- app/core/config.py
+++ app/core/config.py
@@ -1,9 +1,12 @@
 import os
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
+
 
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     PROJECT_NAME: str = "Lu Estilo API"
+
+    DATABASE_URL: str
     
     SECRET_KEY: str = os.getenv("SECRET_KEY", "supersecretkey")
     ALGORITHM: str = "HS256"
